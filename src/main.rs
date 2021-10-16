48627ff651f3e373942cd5490af97f86716432fc
Francis Nicholson <francisnicholson@gmail.com>
2021-10-16 17:26:34 +0200

    resize feature added, untested

--- src/main.rs
+++ src/main.rs
@@ -1,23 +1,34 @@
-use i3ipc::{reply::NodeLayout, I3Connection, I3EventListener, Subscription, event::Event, event::inner::WindowChange};
+use i3ipc::{reply::NodeLayout, I3Connection, I3EventListener, Subscription, event::{Event, WindowEventInfo}, event::inner::WindowChange};
 
 fn main() -> Result<(), Box<dyn std::error::Error>> {
     let mut connection = I3Connection::connect()?;
     let mut listener = I3EventListener::connect()?;
-    let subs = [Subscription::Window];
+    let subs = [Subscription::Window, Subscription::Binding];
     listener.subscribe(&subs)?;
     for event in listener.listen() {
-        if let Event::WindowEvent(info) = event?  {
-            match info.container.layout {
-                NodeLayout::SplitH | NodeLayout::SplitV if info.change == WindowChange::New => {
-                    let (_, _, w, h) = info.container.window_rect;
-                    if w > h {
-                        connection.run_command("split h")?;
-                    } else {
-                        connection.run_command("split v")?;
-                    }
-                },
-                _ => (),
-            }
+        let relevant_event = |i : &WindowEventInfo| 
+            i.container.layout == NodeLayout::SplitH 
+            || i.container.layout == NodeLayout::SplitV
+            && i.change == WindowChange::Focus;
+
+        let (_, _, w, h) = match event? {
+            Event::WindowEvent(info) if relevant_event(&info) => {
+                info.container.window_rect
+            },
+            Event::BindingEvent(binding) if binding.binding.command.starts_with("resize") => {
+                let mut node = &connection.get_tree()?;
+                while !node.focused {
+                    node = node.nodes.iter().find(|n| node.focus.first().map(|&id| id == n.id).unwrap_or(false)).expect("no focused node");
+                }
+                node.window_rect
+            },
+            _ => continue,
+        };
+
+        if w > h {
+            connection.run_command("split h")?;
+        } else {
+            connection.run_command("split v")?;
         }
     }
     Ok(())
